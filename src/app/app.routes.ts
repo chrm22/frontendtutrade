1249fda2d82034a1c5accb9d7581bee41f9ddd36
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-24 01:13:07 -0500

    Actualización de AppComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './componente/home/home.component';
+import {UsuarioRegistroComponent} from './componente/usuario-registro/usuario-registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'registro', component: UsuarioRegistroComponent },
+];
