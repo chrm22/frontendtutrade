4d8001e314996403fa16e8fa1da06d32bd767a3e
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-28 02:08:40 -0500

    Actualización de rutas para incluir canActivate y diferenciar funciones de aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,22 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './componente/home/home.component';
 import {UsuarioRegistroComponent} from './componente/usuario-registro/usuario-registro.component';
 import {LoginComponent} from './componente/login/login.component';
+import {FeedComponent} from './componente/feed/feed.component';
+import {AuthGuard} from './guards/auth.guard';
+import {AuthRedirectGuard} from './guards/auth-redirect.guard';
+import {DynamicLayoutComponent} from './componente/dynamic-layout/dynamic-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'registro', component: UsuarioRegistroComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthRedirectGuard] },
+  { path: 'registro', component: UsuarioRegistroComponent, canActivate: [AuthRedirectGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthRedirectGuard] },
+
+  {
+    path: 'app',
+    component: DynamicLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'feed', component: FeedComponent, canActivate: [AuthGuard] },
+    ]
+  },
 ];
