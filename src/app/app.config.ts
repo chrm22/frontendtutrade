3bbe186967a02031eccdc94f14c57bcb33463409
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-28 02:09:08 -0500

    Implementación de guard y cambio de formato de fecha

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient} from "@angular/common/http";
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {AuthInterceptor} from './interceptors/auth.interceptor';
+import {AuthGuard} from './guards/auth.guard';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    AuthGuard]
 };
