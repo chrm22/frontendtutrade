0f111ad17ab29917111d4cfb0c5c6ba949e558c8
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-28 02:03:49 -0500

    Implementación de función isTokenExpired para verificar vigencia del token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import {environment} from '../../environments/environment';
+import {Token} from '../model/token';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +14,38 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  isTokenExpired(token: string): boolean {
+    if (!token) return true;
+    const decoded: any = jwtDecode(token);
+    const expirationDate = new Date(0);
+    expirationDate.setUTCSeconds(decoded.exp);
+      return expirationDate < new Date();
+  }
+
   login(username: string, password: string): Observable<any> {
     return this.http.post<any>(this.apiUrl, { username, password })
       .pipe(
-        tap(response => {
-          // Almacenar el JWT en localStorage o sessionStorage
-          localStorage.setItem('token', response.token);
+        tap((data: Token) => {
+          localStorage.setItem("jwtToken", data.jwtToken);
+          localStorage.setItem("id", data.id.toString());
+          localStorage.setItem("authorities", data.authorities);
         })
       );
   }
 
   logout() {
-    // Eliminar el token del almacenamiento
-    localStorage.removeItem('token');
+    localStorage.clear();
   }
 
-  getToken(): string | null {
-    return localStorage.getItem('token');
+  getId() {
+    return localStorage.getItem("id");
+  }
+
+  getToken() {
+    return localStorage.getItem("jwtToken");
+  }
+
+  getAuthorities() {
+    return localStorage.getItem("authorities");
   }
 }
