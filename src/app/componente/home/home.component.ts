ebe4657389c81e34a29050276ac892275556bc42
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-28 02:10:22 -0500

    Implementación de navbar propia de home

--- src/app/componente/home/home.component.ts
+++ src/app/componente/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {MatIcon} from "@angular/material/icon";
+import {MatAnchor, MatIconButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatIcon,
+    MatIconButton,
+    MatToolbar,
+    MatAnchor
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
