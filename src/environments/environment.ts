521805271ea817e6746762057a63ca3df51560bb
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-29 03:11:20 -0500

    Implementación de variables de Cloudinary en entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
+  CLOUDINARY_UPLOAD_PRESET: 'cloudinary-articulos',
+  CLOUDINARY_CLOUD_NAME: 'dcsffd8eq',
+  CLOUDINARY_API_KEY: '594345216541931',
+  CLOUDINARY_API_SECRET: 'h9wy9wPf8IZH2L7O_ZzhIv4jKlY',
 }
