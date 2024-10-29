37080e8793dfbe78efe2a17694f6a0593b0e70ee
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-29 03:10:39 -0500

    Creación de componente para perfil de usuario

---
+++ src/app/componente/usuario-perfil/usuario-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-perfil.component.html',
+  styleUrl: './usuario-perfil.component.css'
+})
+export class UsuarioPerfilComponent {
+
+}
