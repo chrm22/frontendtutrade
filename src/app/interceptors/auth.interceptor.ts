8da3c384a49c0a555d700ef51a4129394846081a
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-10-29 03:13:51 -0500

    Implementación de logs a interceptor y adición de ruta cloudinary a rutas excluidas

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,15 +2,23 @@ import {inject, Injectable} from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private authService: AuthService = inject(AuthService);
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Interceptor ejecutado');
     const token = this.authService.getToken();
 
-    const excludedUrls = ['/api/usuarios/register', '/api/usuarios/login'];
+    const cloudinaryUrl = 'https://api.cloudinary.com/v1_1/' + environment.CLOUDINARY_CLOUD_NAME + '/image/upload'
+
+    const excludedUrls = [
+      '/api/usuarios/register',
+      '/api/usuarios/login',
+      cloudinaryUrl
+    ];
 
     if (excludedUrls.some(url => req.url.includes(url))) {
       return next.handle(req);
@@ -20,8 +28,11 @@ export class AuthInterceptor implements HttpInterceptor {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`)
       });
+      console.log('Token insertado de manera correcta');
+      console.log(token);
       return next.handle(cloned);
     } else {
+      console.error('Token inválido o caducado');
       return next.handle(req);
     }
   }
