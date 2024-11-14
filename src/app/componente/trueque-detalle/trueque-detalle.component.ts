374da1fa51e8e7624f70f2707341f10b22536a9c
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 08:31:27 -0500

    Creación de componente de detalle de trueque

---
+++ src/app/componente/trueque-detalle/trueque-detalle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trueque-detalle',
+  standalone: true,
+  imports: [],
+  templateUrl: './trueque-detalle.component.html',
+  styleUrl: './trueque-detalle.component.css'
+})
+export class TruequeDetalleComponent {
+
+}
