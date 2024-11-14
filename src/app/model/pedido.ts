5fc49d8b870984d30d38111b9fb3b8b8f1bab9bd
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 01:50:27 -0500

    Creación de entidad de "Pedido"

---
+++ src/app/model/pedido.ts
@@ -0,0 +1,11 @@
+import {Articulo} from './articulo';
+
+export class Pedido {
+  id: number;
+  articulo: Articulo;
+  articuloOfrecido: Articulo;
+  fechaCreacion: Date;
+  estado: string;
+
+  articuloOfrecidoId: number;
+}
