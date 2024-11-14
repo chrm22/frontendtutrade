3176e196759e8de52e0fb6826332e590c1ac12f8
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 02:07:49 -0500

    Implementación de rutas para ver artículo por ID, ver mis artículos y ver propuestas enviadas y recibidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,10 @@ import {AuthRedirectGuard} from './guards/auth-redirect.guard';
 import {DynamicLayoutComponent} from './componente/dynamic-layout/dynamic-layout.component';
 import {ArticuloRegistroComponent} from './componente/articulo-registro/articulo-registro.component';
 import {UsuarioPerfilComponent} from './componente/usuario-perfil/usuario-perfil.component';
+import {ArticuloDetalleComponent} from './componente/articulo-detalle/articulo-detalle.component';
+import {MisArticulosComponent} from './componente/mis-articulos/mis-articulos.component';
+import {TruequeRecibidasComponent} from './componente/trueque-recibidas/trueque-recibidas.component';
+import {TruequeEnviadasComponent} from './componente/trueque-enviadas/trueque-enviadas.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthRedirectGuard] },
@@ -22,6 +26,10 @@ export const routes: Routes = [
       { path: 'feed', component: FeedComponent },
       { path: 'registrar-articulo', component: ArticuloRegistroComponent },
       { path: 'perfil', component: UsuarioPerfilComponent },
+      { path: 'articulo/:id', component: ArticuloDetalleComponent },
+      { path: 'mis-articulos', component: MisArticulosComponent },
+      { path: 'propuestas/enviadas', component: TruequeEnviadasComponent },
+      { path: 'propuestas/recibidas', component: TruequeRecibidasComponent },
     ]
   },
 ];
