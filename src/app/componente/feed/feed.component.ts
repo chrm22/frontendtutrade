375284fc73595be3f84ba6df37cf7110c4c8f6c6
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 01:57:31 -0500

    Implementación de lógica para componente Feed

--- src/app/componente/feed/feed.component.ts
+++ src/app/componente/feed/feed.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {ArticuloService} from '../../services/articulo.service';
+import {Articulo} from '../../model/articulo';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardModule
+} from '@angular/material/card';
+import {UploadImageService} from '../../services/upload-image.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-feed',
@@ -9,11 +22,52 @@ import {MatIconButton} from '@angular/material/button';
   imports: [
     MatToolbar,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    NgOptimizedImage,
+    NgForOf,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardImage,
+    MatCardActions,
+    MatButton,
+    MatCardModule,
+    RouterLink
   ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
 export class FeedComponent {
 
+  protected articulos: Articulo[];
+  private articuloService: ArticuloService = inject(ArticuloService);
+  private imagenService: UploadImageService = inject(UploadImageService);
+  placeholderImage: string = "assets/placeholder-image.svg"
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.articuloService.list().subscribe(data => {
+      this.articulos = data;
+
+      this.articulos.forEach(articulo => {
+        const imageUrl = articulo.imagenes?.[0]?.url;
+
+        if (imageUrl) {
+          // Verificamos si la imagen existe
+          this.imagenService.checkImageExistence(imageUrl).subscribe(exists => {
+            if (!exists) {
+              articulo.imagenes[0].url = this.placeholderImage;  // Asignamos el placeholder si la imagen no existe
+            }
+          });
+        } else {
+          // Si no hay URL, asignamos directamente el placeholder
+          articulo.imagenes[0].url = this.placeholderImage;
+        }
+      });
+    });
+
+
+
+  }
 }
