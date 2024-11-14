2b55a6093ed5cefa60a6279a4cd7c1ee05bafa1e
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 02:07:13 -0500

    Implementación de componente para listar propuestas de trueque recibidas

---
+++ src/app/componente/trueque-recibidas/trueque-recibidas.component.ts
@@ -0,0 +1,61 @@
+import {Component, inject} from '@angular/core';
+import {Pedido} from '../../model/pedido';
+import {ArticuloService} from '../../services/articulo.service';
+import {PedidoService} from '../../services/pedido.service';
+import {MatCard, MatCardContent, MatCardSmImage} from '@angular/material/card';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-trueque-recibidas',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardSmImage,
+    NgForOf,
+    NgOptimizedImage
+  ],
+  templateUrl: './trueque-recibidas.component.html',
+  styleUrl: './trueque-recibidas.component.css'
+})
+export class TruequeRecibidasComponent {
+  misRecibidas: Pedido[];
+  articuloService: ArticuloService = inject(ArticuloService);
+  pedidoService: PedidoService = inject(PedidoService);
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.pedidoService.getPedidosRecibidos().subscribe({
+      next: data => {
+        this.misRecibidas = data;
+
+        this.misRecibidas.forEach(pedido => {
+
+          this.articuloService.getById(pedido.articulo.id).subscribe({
+            next: data => {
+              pedido.articulo = data;
+            },
+            error: error => {
+              console.log(error);
+            }
+          })
+
+          this.articuloService.getById(pedido.articuloOfrecido.id).subscribe({
+            next: data => {
+              pedido.articuloOfrecido = data;
+            },
+            error: error => {
+              console.log(error);
+            }
+          })
+
+        })
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+}
