73ffd94a9e06eb474e17720df7d8425d238abdca
chrm22 <128739198+chrm22@users.noreply.github.com>
2024-11-14 01:50:46 -0500

    Creación de servicio de "Pedido"

---
+++ src/app/services/pedido.service.ts
@@ -0,0 +1,40 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {Usuario} from '../model/usuario';
+import {Pedido} from '../model/pedido';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+
+  private url = environment.apiUrl;
+  private http: HttpClient = inject(HttpClient);
+  private listaCambio = new Subject<Usuario[]>();
+
+  constructor() { }
+
+  crearPedido(pedido: Pedido, idArticulo: number): Observable<any> {
+    return this.http.post(`${this.url}/articulos/${idArticulo}`, pedido);
+  }
+
+  getPedidosRecibidos(): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(this.url + "/pedidos/recibidos");
+  }
+
+  getPedidosRecibidosPorArticulo(idArticulo: number): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${this.url}/pedidos/recibidos/${idArticulo}`);
+  }
+
+  getPedidosRealizados(): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(this.url + "/pedidos/realizados");
+  }
+
+  getPedidosRealizadosPorArticulo(idArticulo: number): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${this.url}/pedidos/realizados/${idArticulo}`);
+  }
+
+
+}
